5ae0f1f7fd790a183ceadc39f621d78d2ec10b5e
natfed2022 <natalia.fedchenko@fluentcommerce.com>
2023-08-29 22:03:16 +1000

    Added L22-Selenium-POM

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,3 @@
-import com.github.hemanthsridhar.CSVUtils;
-import com.github.hemanthsridhar.lib.ExtUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,16 +9,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import pages.BasePage;
+import pages.LoginPage;
+import pages.SongsPage;
 
 import java.time.Duration;
 import java.util.UUID;
 
 public class BaseTest {
-    public WebDriver driver = null;
+    public static WebDriver driver = null;
+    public static WebDriverWait wait = null;
     public static Actions actions = null;
-//    public String url = "https://qa.koel.app/";
-    public String url;
-    WebDriverWait wait;
+    BasePage basePage ;
+    LoginPage loginPage;
+    SongsPage songsPage ;
 
     @BeforeSuite
     static void setupClass() {
@@ -28,74 +30,75 @@ public class BaseTest {
     }
 
     @BeforeClass
-    @Parameters({"BaseURL"})
-    public void launchBrowser(String baseURL) {
+    public void initTest() {
         //Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         options.addArguments("--disable-notifications");
         options.addArguments("--start-maximized");
-        url=baseURL;
         driver = new ChromeDriver(options);
         //instantiate Explicit wait
         wait = new WebDriverWait(driver,Duration.ofSeconds(20));
         actions = new Actions(driver);
-
+        basePage = new BasePage(driver,wait,actions);
+        basePage.navigateToPage("https://qa.koel.app/");
+        loginPage = new LoginPage(driver,wait,actions);
+        songsPage = new SongsPage(driver,wait,actions);
     }
 
     @AfterClass
-    public void closeBrowser() {
-        driver.quit();
+    public void teardown() {
+        basePage.quitBrowser();
     }
-
-    public void navigateToPage() {
-        driver.get(url);
-    }
-
-    public void provideEmail(String email) {
-        enterText(By.cssSelector("input[type='email']"),email);
-    }
-
-    public void providePassword(String password) {
-        enterText(By.cssSelector("input[type='password']"),password);
-    }
-
-    public void clickSubmit() {
-        clickOnElement(By.cssSelector("[type='submit']"));
-    }
-
-    public String generateRandomName() {
-        return UUID.randomUUID().toString().replace("-", "");
-    }
-    protected void loginCorrectCred() {
-        navigateToPage();
-        provideEmail("demo@class.com");
-        providePassword("te$t$tudent");
-        clickSubmit();
-    }
-
-    void clickOnElement(By locator){
-        WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
-        el.click();
-    }
-    void enterText(By locator, String text){
-        WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
-        el.click();
-        el.clear();
-        el.sendKeys(text);
-    }
-
-    private void clickOnOk() {
-        WebElement okBtn = driver.findElement(By.cssSelector(".ok"));
-        okBtn.click();
-    }
-
-    protected void checkShowSuccess() {
-        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
-       Assert.assertTrue(notification.isDisplayed());
-    }
-
-    protected void clickOnPlaylist(String playlistName) {
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'" + playlistName + "')]"))).click();
-    }
+//
+//    public void navigateToPage() {
+//        driver.get(url);
+//    }
+//
+//    public void provideEmail(String email) {
+//        enterText(By.cssSelector("input[type='email']"),email);
+//    }
+//
+//    public void providePassword(String password) {
+//        enterText(By.cssSelector("input[type='password']"),password);
+//    }
+//
+//    public void clickSubmit() {
+//        clickOnElement(By.cssSelector("[type='submit']"));
+//    }
+//
+//    public String generateRandomName() {
+//        return UUID.randomUUID().toString().replace("-", "");
+//    }
+//    protected void loginCorrectCred() {
+//        navigateToPage();
+//        provideEmail("demo@class.com");
+//        providePassword("te$t$tudent");
+//        clickSubmit();
+//    }
+//
+//    void clickOnElement(By locator){
+//        WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
+//        el.click();
+//    }
+//    void enterText(By locator, String text){
+//        WebElement el= wait.until(ExpectedConditions.elementToBeClickable(locator));
+//        el.click();
+//        el.clear();
+//        el.sendKeys(text);
+//    }
+//
+//    private void clickOnOk() {
+//        WebElement okBtn = driver.findElement(By.cssSelector(".ok"));
+//        okBtn.click();
+//    }
+//
+//    protected void checkShowSuccess() {
+//        WebElement notification = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.success.show")));
+//       Assert.assertTrue(notification.isDisplayed());
+//    }
+//
+//    protected void clickOnPlaylist(String playlistName) {
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[contains(text(),'" + playlistName + "')]"))).click();
+//    }
 }
